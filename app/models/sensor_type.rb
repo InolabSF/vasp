a2798c387d36bcecbf74e3485f241c2c2a94b771
kenzan8000 <kenzan8000@gmail.com>
2015-10-22 09:28:56 -0700

    add sensor_type

--- app/models/sensor_type.rb
+++ app/models/sensor_type.rb
@@ -1,5 +1,7 @@
 class SensorType < ActiveHash::Base
   self.data = [
-    { :id => 1,  :name => 'humidity' }
+    { :id => 1,  :name => 'humidity' },
+    { :id => 2,  :name => 'co' },
+    { :id => 3,  :name => 'living_risk_index' }
   ]
 end
