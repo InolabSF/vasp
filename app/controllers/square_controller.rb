89e140e448e538689d5ec52356cafc32fd4732f9
kenzan8000 <kenzan8000@gmail.com>
2016-02-17 16:00:04 -0800

    update square_data_collection_task to collect each square sizes

--- app/controllers/square_controller.rb
+++ app/controllers/square_controller.rb
@@ -5,14 +5,25 @@ class SquareController < ApplicationController
   def get
     do_return_json = true
 
-    #south = params[:south].to_f
-    #north = params[:north].to_f
-    #west = params[:west].to_f
-    #east = params[:east].to_f
-    #do_return_json = false if (!(south.is_a?(Float) || south.is_a?(Fixnum)) || !(north.is_a?(Float) || north.is_a?(Fixnum)) || !(west.is_a?(Float) || west.is_a?(Fixnum)) || !(east.is_a?(Float) || east.is_a?(Fixnum)))
+    radius = params[:radius].to_f
+    radius = 0.0025 if radius == nil || radius < 0.0025
+
+#    south = params[:south].to_f
+#    north = params[:north].to_f
+#    west = params[:west].to_f
+#    east = params[:east].to_f
+#    do_return_json = false if (!(south.is_a?(Float) || south.is_a?(Fixnum)) ||
+#                               !(north.is_a?(Float) || north.is_a?(Fixnum)) ||
+#                               !(west.is_a?(Float)  || west.is_a?(Fixnum))  ||
+#                               !(east.is_a?(Float)  || east.is_a?(Fixnum)))
 
     if do_return_json
-      squares = Square.all
+      squares = Square.where(
+        radius: radius
+        #lat: south..north,
+        #lng: west..east
+      )
+
       json = Jbuilder.encode do |j|
         j.application_code(200)
         j.squares(squares)
