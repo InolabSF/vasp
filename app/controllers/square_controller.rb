9a74f02a5cf2312fc20abceceeccd1afc1d53173
kenzan8000 <kenzan8000@gmail.com>
2016-02-24 11:11:59 -0800

    update square_controller

--- app/controllers/square_controller.rb
+++ app/controllers/square_controller.rb
@@ -7,17 +7,15 @@ class SquareController < ApplicationController
     radius = params[:radius].to_f
     radius = 0.001 if radius == nil || radius < 0.001
 
-    ## range to get
-    #south = params[:south] ? params[:south].to_f : -90.0
-    #north = params[:north] ? params[:north].to_f : 90.0
-    #west = params[:west] ? params[:west].to_f : -180.0
-    #east = params[:east] ? params[:east].to_f : 180.0
+    # range to get
+    south = params[:south] ? params[:south].to_f : -90.0
+    north = params[:north] ? params[:north].to_f : 90.0
+    west = params[:west] ? params[:west].to_f : -180.0
+    east = params[:east] ? params[:east].to_f : 180.0
 
-    squares = Square.where(
-      radius: radius#,
-      #lat: south..north,
-      #lng: west..east
-    )
+    squares = Square.where(radius: radius, lat: south..north, lng: west..east)
+    puts squares.length
+    #squares = Square.where(radius: radius)
 
     json = Jbuilder.encode do |j|
       j.application_code(200)
